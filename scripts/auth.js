836aa5503e38afa4bc797d8e0900ece3dc44f59f
vikash2806 <vikashrajendran2806@gmail.com>
2021-04-14 11:36:10 +0530

    logout added

--- scripts/auth.js
+++ scripts/auth.js
@@ -15,3 +15,11 @@ singUpForm.addEventListener('submit', (e) => {
             M.Modal.getInstance(modal).close();
       });
 });
+// !Logout user 
+const logout = document.querySelector('#logout');
+logout.addEventListener('click',(e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log('user logged out');
+    })
+})
