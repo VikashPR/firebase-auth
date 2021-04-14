2771826dffe4ee0dc442e538f72d63ef4ff6cd9e
vikash2806 <vikashrajendran2806@gmail.com>
2021-04-14 11:24:12 +0530

    Initial commit

---
+++ scripts/auth.js
@@ -0,0 +1,17 @@
+const singUpForm = document.querySelector('#signup-form');
+singUpForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Get user Info
+    const email = singUpForm['signup-email'].value;
+    const password = singUpForm['signup-password'].value;
+    // console.log("emailasd:",email,"passownet:", password);
+
+    // !Sign up with firebase auth
+      auth.createUserWithEmailAndPassword(email, password).then(cred => {
+          console.log(cred);
+        const modal = document.querySelector('#modal-signup');
+            singUpForm.reset();
+            M.Modal.getInstance(modal).close();
+      });
+});
